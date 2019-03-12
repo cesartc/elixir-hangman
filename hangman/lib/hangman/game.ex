ec7e52e884b9d88e038a48050ee8b25420c40e15
Cesar <cesartarrillocelada@gmail.com>
2019-03-12 17:09:33 -0500

    test(Hangman): Test new game

--- hangman/lib/hangman/game.ex
+++ hangman/lib/hangman/game.ex
@@ -3,9 +3,10 @@ defmodule Hangman.Game do
   defstruct(
     turns_left: 7,
     game_state: :initializing,
+    letters: [],
   )
 
   def new_game() do
-    %Hangman.Game{}
+    %Hangman.Game{letters: Dictionary.random_word |> String.codepoints}
   end
 end
