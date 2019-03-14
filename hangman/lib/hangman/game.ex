0aa31f0b3d19d48d30b94bf4abc3a71b7a4d65a8
Cesar <cesartarrillocelada@gmail.com>
2019-03-14 11:48:22 -0500

    feat(hangman): Make move MapSet

--- hangman/lib/hangman/game.ex
+++ hangman/lib/hangman/game.ex
@@ -4,9 +4,41 @@ defmodule Hangman.Game do
     turns_left: 7,
     game_state: :initializing,
     letters: [],
+    used: MapSet.new
   )
 
   def new_game() do
     %Hangman.Game{letters: Dictionary.random_word |> String.codepoints}
   end
+
+  # def make_move(game = %{ game_state: :won }, _) do
+  #   { game, current_score(game) }
+  # end
+
+  # def make_move(game = %{ game_state: :lost }, _) do
+  #   { game, current_score(game) }
+  # end
+
+  def make_move(game = %{ game_state: state }, _) when state in [:won, :lost] do
+    { game, current_score(game) }
+  end
+
+  def make_move(game, guess) do
+    #if MapSet.member?(game, guess) do
+    game = accept_move(game, guess, MapSet.member?(game.used, guess))
+    { game, current_score(game)}
+  end
+
+  def accept_move(game, guess, _already_guessed = true) do
+    Map.put(game, :game_state, :already_used)
+  end
+
+  def accept_move(game, guess, _already_guessed) do
+    Map.put(game, :used, MapSet.put(game.used, guess))
+  end
+
+  def current_score(game) do
+    123
+  end
+
 end
--- hangman/test/game_test.exs
+++ hangman/test/game_test.exs
@@ -20,4 +20,47 @@ defmodule GameTest do
     assert word == String.downcase(word)
   end
 
+  test "state isn't changed for :won game" do
+    game = Game.new_game
+    game = Map.put(game, :game_state, :won)
+    { new_game, _ } = Game.make_move(game, 'whatever')
+    assert new_game == game
+  end
+
+  test "state isn't changed for :won game [refactored]" do
+    game = Game.new_game
+      |> Map.put(:game_state, :won)
+
+    assert { ^game, _ } = Game.make_move(game, 'whatever')
+  end
+
+  test "state isn't changed for :lost game [refactored]" do
+    game = Game.new_game
+      |> Map.put(:game_state, :lost)
+    assert { ^game, _ } = Game.make_move(game, 'whatever')
+  end
+
+  test "state isn't changed for :won or :lost game [refactored]" do
+    for state <- [:won, :lost] do
+      game = Game.new_game
+        |> Map.put(:game_state, state)
+      assert { ^game, _ } = Game.make_move(game, 'whatever')
+    end
+  end
+
+
+  test "first occurence of letter is not already used" do
+    game = Game.new_game
+    {game, score} = Game.make_move(game, "a")
+    assert game.game_state != :already_used
+  end
+
+  test "second occurence of letter is already used" do
+    game = Game.new_game
+    {game, score} = Game.make_move(game, "a")
+    {game, score} = Game.make_move(game, "a")
+    assert game.game_state == :already_used
+  end
+
+
 end
